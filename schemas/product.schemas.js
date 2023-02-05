1f951542ae04d9e79ef36c21160386b07ff46140
Johnnatan Reyes <tanamix22@gmail.com>
2023-02-05 18:13:56 +0000

    fix: fix format of idProduct

--- schemas/product.schemas.js
+++ schemas/product.schemas.js
@@ -4,7 +4,7 @@ const productId = Joi.string().uuid();
 const name = Joi.string().min(3).max(15);
 const price = Joi.number().integer().min(10);
 const image = Joi.string().uri();
-const idProduct = Joi.number().integer();
+const idProduct = Joi.string().uuid();
 const StorageId = Joi.string();
 const colorId = Joi.string();
 
